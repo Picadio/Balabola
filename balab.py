b98a1c02fcdceda2f846149cc90f30b5b3f89855
Picadio <56791168+Picadio@users.noreply.github.com>
2021-07-29 18:18:30 +0300

    Update balab.py

--- balab.py
+++ balab.py
@@ -5,7 +5,7 @@ from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
 from webdriver_manager.utils import ChromeType
 
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+driver = webdriver.Chrome(ChromeDriverManager.install())
 
 driver.get('https://yandex.ru/lab/yalm')
 
