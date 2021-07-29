605cfb9ffd99c01c0e81c353bf7ae0e51c0c46c9
Picadio <56791168+Picadio@users.noreply.github.com>
2021-07-29 18:49:58 +0300

    Update balab.py

--- balab.py
+++ balab.py
@@ -15,7 +15,8 @@ login = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[3]/span/span
 
 button = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/button')
 driver.execute_script("arguments[0].click();", button)
-
+button = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/button')
+driver.execute_script("arguments[0].click();", button)
 
 soup = BeautifulSoup(driver.page_source, 'lxml')
 print(soup.span)
