d17f8b3f56bdb802a9f1ecdd9a2fd53a2c6f08b3
Picadio <56791168+Picadio@users.noreply.github.com>
2021-07-29 17:55:55 +0300

    Update balab.py

--- balab.py
+++ balab.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
-
+from bs4 import BeautifulSoup
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -11,7 +11,11 @@ driver.get('https://yandex.ru/lab/yalm')
 
 cl = driver.find_element_by_xpath('//*[@id="app"]/div/div/button').click()
 
-login = driver.find_element_by_xpath("//*[@id="app"]/div/div[2]/div[3]/span/span[2]/textarea").send_keys("Hello")
+login = driver.find_element_by_xpath("//*[@id="app"]/div/div[2]/div[3]/span/span[2]/textarea").send_keys("Привет как дела")
 
 button = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/button')
 button.click()
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+print(soup.textarea)
+
